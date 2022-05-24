dba9ce0fe900dfafe9c5642b56e67fcbc4c87779
Горбунов Владимир <42053880+fgreeeee@users.noreply.github.com>
2022-05-24 20:17:20 +0900

    Add files via upload

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="p-3 bg-light text-dark">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+          <svg class="bi me-2" width="50" height="32"  aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="/" class="nav-link px-2 text-dark">Главная</a></li>
+          <li><a href="/table_phys" class="nav-link px-2 text-dark">Таблица</a></li>
+        </ul>
+
+        <div class="text-end" >
+          <a href="/login" button type="button" class="btn btn-outline-dark me-2">Войти</a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MyHeader"
+}
+</script>
+
+<style scoped>
+.nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0{
+  background-color: darkslateblue;
+}
+</style>
---
+++ src/pages/MyHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img src="https://catherineasquithgallery.com/uploads/posts/2021-02/1612170598_69-p-fon-gradient-sinii-fioletovii-83.jpg" class="img-fluid" alt="Responsive image">
+  </div>
+  <div class="position-absolute top-50 start-50 translate-middle" style="font-size: 60px; color: white">
+    <h1>Добро пожаловать в админ панель!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHome"
+}
+</script>
+<style>
+img {
+  position: absolute;
+  filter: brightness(50%);
+}
+
+</style>
